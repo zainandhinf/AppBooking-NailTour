cadaa3206d72bef3906972429a4b57d36af57078
zainandhinf <zainandhif@gmail.com>
2023-10-08 19:22:02 +0700

    membuat isi model admin

--- database/migrations/2023_10_08_115719_create_admins_table.php
+++ database/migrations/2023_10_08_115719_create_admins_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,15 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
+            $table->string('name');
+            $table->string('password');
+            $table->string('email');
+            $table->string('no_phone');
+            $table->string('gender');
+            $table->string('religion');
+            $table->string('date_birth');
+            $table->string('pic');
             $table->timestamps();
         });
     }
